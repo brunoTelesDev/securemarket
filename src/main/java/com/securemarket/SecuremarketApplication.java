6f5c47e089b75bc0c67dfaccbefdea1eb2aef672
brunoTelesDev <brunoteles.dev@gmail.com>
2026-05-15 23:44:55 -0300

    chore: setup inicial com docker-compose do banco e application.yml

---
+++ src/main/java/com/securemarket/SecuremarketApplication.java
@@ -0,0 +1,13 @@
+package com.securemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuremarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuremarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securemarket/SecuremarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuremarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
